6df179b90d9a4a51c043087437c151b4f642ae27
waywho <walzerfan@yahoo.com>
2017-05-31 21:23:20 +0000

    add nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <div>
+                Nav Component
+            </div>
+        );
+    }
+
+}
+
+export default Nav;
