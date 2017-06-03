07677f58fd3373c469656d39f0a2c6649d1d683a
waywho <walzerfan@yahoo.com>
2017-06-03 11:05:56 +0000

    add render tag input

--- src/components/Note.js
+++ src/components/Note.js
@@ -22,6 +22,16 @@ class Note extends React.Component {
                     </i>
                 </span>
             )
+        } else {
+            return(
+                    <form>
+                    <input
+                        className="tag-input"
+                        type="text"
+                        placeholder="Tag Name..."
+                    />
+                </form> 
+            )
         }
     }
 
